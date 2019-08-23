7ce2da5f017829fca7dab906b839197b81871795
Raajn Patel <rndpatel@gmail.com>
2019-08-23 10:20:20 -0700

    Stretch started, added to SearchForm.js

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,18 +1,25 @@
-// import React, { useState } from "react";
-//
-// export default function SearchForm({ onSearch }) {
-//   // STRETCH TODO: Add stateful logic for query/form data
-//   return (
-//     <section className="search-form">
-//       <form onSubmit={() => onSearch(name)}>
-//         <input
-//           onChange={handleInputChange}
-//           placeholder="name"
-//           value={name}
-//           name="name"
-//         />
-//         <button type="submit">Search</button>
-//       </form>
-//     </section>
-//   );
-// }
+import React, { useState } from "react";
+
+export default function SearchForm({ onSearch }) {
+  // STRETCH TODO: Add stateful logic for query/form data
+    const [query, setQuery] = useState("");
+    const handleInputChange = (event) => {
+        console.log(event.target.name);
+        console.log(event.target.value);
+        setQuery(event.target.value)
+    };
+
+  return (
+    <section className="search-form">
+      <form onSubmit={(event) => onSearch(event, query)}>
+        <input
+          onChange={handleInputChange}
+          placeholder="name"
+          value={query}
+          name="name"
+        />
+        <button type="submit">Search</button>
+      </form>
+    </section>
+  );
+}

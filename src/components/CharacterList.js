d860ad89132f46d9aee736f23726fbffa2a51a18
Raajn Patel <rndpatel@gmail.com>
2019-08-23 10:39:34 -0700

    Stretch Goal (Search Bar) complete ReadMe Questions Answered!

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -6,19 +6,26 @@ import SearchForm from "./SearchForm";
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
   //characters will be stored in an array
-  const[characters, setCharacters]=useState([])
+  const[characters, setCharacters]=useState([]);
+  const[name, setName]=useState("");
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
     axios
-        .get(`https://rickandmortyapi.com/api/character/?name=`)
+        .get(`https://rickandmortyapi.com/api/character/?name=${name}`)
         .then(response => {setCharacters(response.data.results)})
         .catch(error => console.log('Unexpected Error: ',error))
-  }, []);
+  }, [name]);
 
+    function onSearch(event, query){
+        event.preventDefault();
+        console.log(event);
+        setName(query);
+    }
 
   return(
       <div>
+          <SearchForm onSearch={onSearch}/>
         <section className='character-list grid-view'>
           {characters.map((character,index) => <CharacterCard key={index} character={character}/>)}
         </section>

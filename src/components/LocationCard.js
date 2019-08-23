c2d7a00dcd8af58e4ba0b5388c0b6b062f64f96f
Raajn Patel <rndpatel@gmail.com>
2019-08-23 09:20:45 -0700

    Location List / Cards made up and all showing. Added styling to flex wrap the cards properly, as well as spacing.  Episodes to be completed next.

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,19 @@
-import React from "react";
+import React from 'react'
+import { Card } from 'semantic-ui-react'
+export default function LocationCard (props) {
+  const {location} = props;
+  return (
 
-export default function LocationCard({ name, type, dimension, residents }) {
-  return <span>todo: location</span>;
+      <Card className="spot">
+        <Card.Content>
+          <Card.Header>{location.name}</Card.Header>
+          <Card.Meta>
+            <span className='date'>Type: {location.type}</span>
+          </Card.Meta>
+          <Card.Description>
+            Dimension:{location.dimension}
+          </Card.Description>
+        </Card.Content>
+      </Card>
+  )
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
 import "./index.css";
 import App from "./App";
-import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(
   <Router>

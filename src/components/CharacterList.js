142922a6f987719ad51aff95fe113bedc4f139ce
Raajn Patel <rndpatel@gmail.com>
2019-08-23 09:07:57 -0700

    Character List / Cards made up and all showing. Location Tab next.

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import CharacterCard from './CharacterCard';
+import SearchForm from "./SearchForm";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-
+  //characters will be stored in an array
+  const[characters, setCharacters]=useState([])
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
+    // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+        .get(`https://rickandmortyapi.com/api/character/?name=`)
+        .then(response => {setCharacters(response.data.results)})
+        .catch(error => console.log('Unexpected Error: ',error))
   }, []);
 
-  return (
-    <section className="character-list grid-view">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
-  );
+
+  return(
+      <div>
+        <section className='character-list grid-view'>
+          {characters.map((character,index) => <CharacterCard key={index} character={character}/>)}
+        </section>
+      </div>
+  )
 }
